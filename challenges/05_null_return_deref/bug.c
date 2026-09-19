cf518bf72548250912fdc002111004579bb0f64f
45sangmin99 <45sangmin99@users.noreply.github.com>
2026-09-19 10:46:21 +0900

    solve 05_null_return_deref

--- challenges/05_null_return_deref/bug.c
+++ challenges/05_null_return_deref/bug.c
@@ -65,6 +65,7 @@ static void expand(const Config *c, const char *tmpl, char *out, size_t outcap) 
             key[kl] = '\0';
 
             const char *v = cfg_get(c, key);      
+            if(!v) v = "";
             size_t vl = strlen(v);                 
             if (o + vl < outcap) { memcpy(out + o, v, vl); o += vl; }
             p = end + 1;
